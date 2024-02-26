8c61b95448f984b53eac1e2d32c36b6b9fc8445f
DForant <dforant@gmail.com>
2024-02-26 13:24:14 -0500

    configure TailwindCSS

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
-import { useState } from 'react'
-
 function App() {
 
   return (
     <main>
-
+        <h3 className='text-6xl text-blue-600 font-semibold text-center'> 
+          Hello World
+        </h3>
     </main>
   )
 }
