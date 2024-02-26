8e27cd63d00f946fa1093bee9a41e46d793d5956
DForant <dforant@gmail.com>
2024-02-26 13:37:44 -0500

    Percent bar component starter code

---
+++ src/Components/PercentBar.jsx
@@ -0,0 +1,15 @@
+const PercentBar = () => {
+    /** TODO: Creeate a percent bar that will take in 3 properties
+     * MaxWidth: maximum width of component
+     * PercentLabel: The Label of the item your putting a percentage on
+     * Percent: integer between 1 and 100
+     * 
+     * To get the px/pct we divide the maxWidth by 100  
+     */
+
+    return (
+        <div>PercentBar</div>
+    )
+}
+
+export default PercentBar
